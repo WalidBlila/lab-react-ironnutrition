8bdc352ea653362fc2ade6c8722812a518afd507
WalidBlila <walidbila8@gmail.com>
2021-06-29 19:55:09 +0100

    ite 5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,128 @@
 import React from 'react';
-
 import './App.css';
-import 'bulma/css/bulma.css'
+import 'bulma/css/bulma.css';
 import foods from './foods.json';
-import FoodBox from './components/FoodBox'
+import FoodBox from './Components/FoodBox';
+import FoodForm from './Components/FoodForm';
+import Search from './Components/Search';
+
 
 class App extends React.Component {
   state = {
-    list : foods
+    foods: foods,
+    searchValue: "",
+    selectedFood: null,
+    todaysFood: [],
+  };
+
+  handleForm = () => {
+    const foodForm = document.getElementById("foodForm");
+    foodForm.classList.toggle("hidden");
   }
-  render(){
-    return (
-      <div className="App">
-      {this.state.list.map((el,i)=>{
-        return <FoodBox key = {i} {...el} />
-      })}
-        
+
+  handleAddFood = (food) => {
+    this.setState({
+      foods: [food, ...this.state.foods],
+    });
+
+    this.handleForm();
+  };
+
+  handleSearchValue = (searchValue) => {
+    this.setState({
+      searchValue: searchValue,
+    });
+  };
+
+  handleSelectFood = (food) => {
+
+    let todaysFoodCopy = [...this.state.todaysFood]
+
+    let found = todaysFoodCopy.find((el) => el.name === food.name);
+
+    food.calories *= food.quantity;
+
+    if (found) {
+      const foundCopy = { ...found };
+      foundCopy.quantity += food.quantity;
+      foundCopy.calories += food.calories;
+
+      this.setState({
+        todaysFood: todaysFoodCopy.map((food) =>
+          food.name === foundCopy.name ? foundCopy : food
+        ),
+      });
+    } else {
+      todaysFoodCopy.push(food);
+      this.setState({
+        todaysFood: todaysFoodCopy,
+      });
+    }
+  };
+
+
+  render() {
+    const filteredFoods = this.state.foods.filter((food) => {
+      return food.name
+        .toLowerCase()
+        .includes(this.state.searchValue.toLowerCase());
+    });
+
+    const totalCalories = this.state.todaysFood.reduce(
+      (accumulator, currentFood) => {
+        return (accumulator += currentFood.calories);
+      },
+      0
+    );
+
+  return (
+    <div className="App">
+
+        <Search
+          value={this.state.searchValue}
+          callbackFn={this.handleSearchValue}
+        />
+
+      <div>
+        <button onClick={this.handleForm}>Add New Food</button>
+        <div id="foodForm" className="hidden" >
+          <FoodForm callbackFn={this.handleAddFood}/>
+        </div>
       </div>
+
+      <div className="flex">
+
+        <div className="columns">
+          <div className="column">
+            {filteredFoods.map((food, index) => {
+              return (
+                <FoodBox
+                  handleSelect={this.handleSelectFood}
+                  key={index}
+                  food={food}
+                />
+              );
+            })}
+          </div>
+
+          <div className="column content">
+            <h2 className="subtitle">Today's foods</h2>
+
+            <ul>
+              {this.state.todaysFood.map((food, index) => (
+                <li key={index}>
+                  {food.quantity} {food.name} = {food.calories} cal
+                </li>
+              ))}
+            </ul>
+
+            <strong>Total: {totalCalories} cal</strong>
+          </div>
+        </div>     
+      </div>
+
+      
+    </div>
     );
   }
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,42 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class FoodBox extends React.Component {
-    state = { 
-        quantity:0
-    }
-    render(){
-        return (
-            <div className="box">
-                <article className="media">
-                    <div className="media-left">
-                    <figure className="image is-64x64">
-                        <img src={this.props.image} alt='' />
-                    </figure>
-                    </div>
-                    <div className="media-content">
-                    <div className="content">
-                        <p>
-                        <strong>{this.props.name}</strong> <br />
-                        <small>{this.props.calories} cal</small>
-                        </p>
-                    </div>
-                    </div>
-                    <div className="media-right">
-                    <div className="field has-addons">
-                        <div className="control">
-                        <input className="input" type="number" value={this.state.quantity} />
-                        </div>
-                        <div className="control">
-                        <button className="button is-info">
-                            +
-                        </button>
-                        </div>
-                    </div>
-                    </div>
-                </article>
+class FoodBox extends Component {
+
+  state = {
+    quantity: 1,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      quantity: event.target.value,
+    });
+  };
+
+  handleClick = () => {
+    const selectedFood = {
+      ...this.props.food,
+      quantity: Number(this.state.quantity),
+    };
+
+    this.props.handleSelect(selectedFood);
+  };
+
+  render() {
+
+    return (
+      <div>
+        <div className="box">
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img src={this.props.food.image} />
+              </figure>
             </div>
-         
-        )
-    }
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{this.props.food.name}</strong> <br />
+                  <small>{this.props.food.calories} cal</small>
+                </p>
+              </div>
+            </div>
+            <div className="media-right">
+              <div className="field has-addons">
+                <div className="control">
+                  <input className="input" type="number" value={this.state.quantity} onChange={this.handleChange} />
+                </div>
+                <div className="control">
+                  <button className="button is-info" onClick={this.handleClick}>
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+        
+      </div>
+    )
+  }
+
 }
+
 export default FoodBox
